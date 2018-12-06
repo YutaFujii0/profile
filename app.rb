44a9747a81b2d2f86f440bb53bb4ad66df29d631
Yuta Fujii <y.f.mar2@gmail.com>
2018-12-06 16:05:27 +0900

    replace code

--- app.rb
+++ app.rb
@@ -4,37 +4,37 @@ require 'uglifier'
 require 'sass'
 require 'sinatra/sprockets-helpers'
 
-class MySinatra < Sinatra::Base
-  register Sinatra::Sprockets::Helpers
-  set :sprockets ,Sprockets::Environment.new
-  set :assets_prefix, '/assets'
-  set :digest_assets, true
-  set :views, [ File.expand_path("../source/views/", __FILE__),File.expand_path("../source/views/layouts", __FILE__) ]
-  set :public_folder, File.expand_path("../source", __FILE__)
+# class MySinatra < Sinatra::Base
+#   register Sinatra::Sprockets::Helpers
+#   set :sprockets ,Sprockets::Environment.new
+#   set :assets_prefix, '/assets'
+#   set :digest_assets, true
+#   set :views, [ File.expand_path("../source/views/", __FILE__),File.expand_path("../source/views/layouts", __FILE__) ]
+#   set :public_folder, File.expand_path("../source", __FILE__)
 
-  configure do
-    sprockets.append_path "source/assets/javascripts"
-    sprockets.append_path "source/assets/stylesheets"
-    sprockets.js_compressor  = :uglify
-    sprockets.css_compressor = :scss
+#   configure do
+#     sprockets.append_path "source/assets/javascripts"
+#     sprockets.append_path "source/assets/stylesheets"
+#     sprockets.js_compressor  = :uglify
+#     sprockets.css_compressor = :scss
 
-    configure_sprockets_helpers do |helpers|
-      helpers.asset_host = 'some-bucket.s3.amazon.com'
-    end
-  end
+#     configure_sprockets_helpers do |helpers|
+#       helpers.asset_host = 'some-bucket.s3.amazon.com'
+#     end
+#   end
 
-  helpers do
-    def find_template(views, name, engine, &block)
-      Array(views).each { |v| super(v, name, engine, &block) }
-    end
-  end
-  get "/" do
-    haml :index, "layouts/layout" => :post
-  end
-  get "/views/index" do
-    haml :index, "layout" => :post
-  end
-  get "/views/layouts/layout" do
-    haml "layouts/layout"
-  end
-end
+#   helpers do
+#     def find_template(views, name, engine, &block)
+#       Array(views).each { |v| super(v, name, engine, &block) }
+#     end
+#   end
+#   get "/" do
+#     haml :index, "layouts/layout" => :post
+#   end
+#   get "/views/index" do
+#     haml :index, "layout" => :post
+#   end
+#   get "/views/layouts/layout" do
+#     haml "layouts/layout"
+#   end
+# end
--- config.rb
+++ config.rb
@@ -79,6 +79,42 @@ configure :build do
   activate :asset_host, :host => '//yutafujii.cloudfront.net'
 end
 
+class MySinatra < Sinatra::Base
+  register Sinatra::Sprockets::Helpers
+  set :sprockets ,Sprockets::Environment.new
+  set :assets_prefix, '/assets'
+  set :digest_assets, true
+  set :views, [ File.expand_path("../source/views/", __FILE__),File.expand_path("../source/views/layouts", __FILE__) ]
+  set :public_folder, File.expand_path("../source", __FILE__)
+
+  configure do
+    sprockets.append_path "source/assets/javascripts"
+    sprockets.append_path "source/assets/stylesheets"
+    sprockets.js_compressor  = :uglify
+    sprockets.css_compressor = :scss
+
+    configure_sprockets_helpers do |helpers|
+      helpers.asset_host = 'some-bucket.s3.amazon.com'
+    end
+  end
+
+  helpers do
+    def find_template(views, name, engine, &block)
+      Array(views).each { |v| super(v, name, engine, &block) }
+    end
+  end
+  get "/" do
+    haml :index, "layouts/layout" => :post
+  end
+  get "/views/index" do
+    haml :index, "layout" => :post
+  end
+  get "/views/layouts/layout" do
+    haml "layouts/layout"
+  end
+end
+
+
 map "/sinatra" do
   run MySinatra
 end
